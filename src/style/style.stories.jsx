d9bb986381d7f415b5b0c3756ae5bc2094e105f6
Nicolas Coen <nicolas.coen@zaelot.com>
2022-11-08 13:37:24 -0300

    Added Foundation stories and addon-docs package

---
+++ src/style/style.stories.jsx
@@ -0,0 +1,10 @@
+const Foundation = () => <div />
+
+export default {
+  title: 'Foundation',
+  component: Foundation
+}
+
+export const Spacing = () => <div>Spacing component</div>
+
+export const Font = () => <div>Font component</div>
