1191ebf256f1a348c9fc7bffe63ea3801068ef98
ruriarte-zaelot <114502056+ruriarte-zaelot@users.noreply.github.com>
2022-12-16 11:30:03 -0300

    add aliases to vite.config.js (#40)

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,3 +1,5 @@
+const { mergeConfig } = require("vite");
+
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
@@ -15,5 +17,16 @@ module.exports = {
   },
   "features": {
     "storyStoreV7": true
-  }
+  },
+  async viteFinal(config) {
+    // Merge custom configuration into the default config
+    return mergeConfig(config, {
+      // Use the same "resolve" configuration as your app
+      resolve: (await import('../vite.config.js')).default.resolve,
+      // Add dependencies to pre-optimization
+      optimizeDeps: {
+        include: [],
+      },
+    });
+  },
 }
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { fileURLToPath, URL } from 'url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,5 +9,13 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: './setupTests.js'
+  },
+  resolve: {
+    alias: {
+      '@assets': fileURLToPath(new URL('./src/assets', import.meta.url)),
+      '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
+      '@style': fileURLToPath(new URL('./src/style', import.meta.url)),
+      '@utils': fileURLToPath(new URL('./src/utils', import.meta.url))
+    }
   }
 })
